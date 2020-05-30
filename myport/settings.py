a93d098484be3f7a02d471dccbad40d24307e612
Elderson Gama <eldesondagama@hotmail.com>
2020-05-29 22:29:52 -0300

    setting heroku - test

--- myport/settings.py
+++ myport/settings.py
@@ -68,7 +68,7 @@ default_dburl = 'sqlite:///' + \
     os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {'default': config(
-    'DATABASEURL', default=default_dburl, cast=dburl)}
+    'DATABASE_URL', default=default_dburl, cast=dburl), }
 
 
 # Password validation
