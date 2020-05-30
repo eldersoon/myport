f39ce5af831b1f1fb3dbb24b92b6deb91f3ffa90
Elderson Gama <eldesondagama@hotmail.com>
2020-05-29 22:06:36 -0300

    setting heroku - test

--- myport/settings.py
+++ myport/settings.py
@@ -109,8 +109,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "public"),
-    '/var/www/static/',
+    os.path.join(BASE_DIR, "public")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
